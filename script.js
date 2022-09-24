5d900c3f94c2efa453c479d5fb2e88edd1fa8fb9
admin <admin@macbook-air-lelaizant.home>
2022-09-24 23:26:16 +0200

    Mise à jour

--- script.js
+++
@@ -1,5 +0,0 @@
-var btn = document.getElementsByClassName("titre")[0].getElementsByClassName("btn")[0];
-btn.addEventListener('click', function() {
-    var couleur = document.getElementsByClassName("titre")[0].getElementsByClassName("couleur")[0];
-    document.getElementsByTagName("body")[0].style.backgroundColor = couleur;
-})
